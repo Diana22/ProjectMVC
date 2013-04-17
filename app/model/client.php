8a0bd848aa8a3ff95a8d0c1ba09451015f82c764
Roman Alina <alina.roman22@yahoo.com>
2013-04-17 13:30:08 +0300

    MVC-6 modified

--- app/model/client.php
+++ app/model/client.php
@@ -9,15 +9,16 @@ class model_client {
     var $phone;
     var $account_id;
 
-    public static function load_by_id($id){
+    public static function load_by_id($client_id){
         $db = model_database::instance();
         $sql = 'SELECT *
                 FROM clients
-                WHERE client_id_account = \'' . $id . '\';';
+                WHERE client_id = \'' . $client_id . '\';';
         if($result = $db->get_row($sql) )
         {
             $obj = new model_client();
-            $obj->id = $result['client_id_account'];
+            $obj->id = $client_id;
+            $obj->account_id = $result['client_id_account'];
             $obj->name = $result['client_name'];
             $obj->address = $result['client_address'];
             $obj->phone = $result['client_phone'];
@@ -28,16 +29,17 @@ class model_client {
 
     public static function load_by_account_id($id_account){
         $db = model_database::instance();
-        $sql = 'SELECT account_id, account_username, account_pass, account_type
-                FROM accounts
-                WHERE account_id = \'' . $id_account . '\';';
+        $sql = 'SELECT *
+                FROM clients
+                WHERE client_id_account = \'' . $id_account . '\';';
         if($result = $db -> get_row($sql) )
         {
-            $obj = new model_account();
-            $obj->id_account = $result['account_id'];
-            $obj->username = $result['account_username'];
-            $obj->pass =$result['account_pass'];
-            $obj->type = $result['account_type'];
+            $obj = new model_client();
+            $obj->id = $result['client_id'];
+            $obj->id_account = $result['client_id_account'];
+            $obj->name = $result['client_name'];
+            $obj->address =$result['client_address'];
+            $obj->phone = $result['client_phone'];
             return $obj;
         }
         return false;
@@ -48,9 +50,10 @@ class model_client {
         $sql = 'INSERT INTO clients
                     (client_id_account, client_name, client_address, client_phone)
                 VALUES
-                    (\'' . $account_id . '\', \'' .  $name .  '\', \'' . $address . '\',\'' .  $phone . '\') ;';
-        if($result = $db->execute($sql)){
-            return model_client::load_by_id($account_id);
+                    (\'' . $account_id . '\', \'' .  mysql_real_escape_string($name) .  '\', \'' . mysql_real_escape_string($address) . '\',\'' .  $phone . '\') ;';
+        if($db->execute($sql)){
+            $new_account_id = $db->last_insert_id();
+            return model_client::load_by_id($new_account_id);
         }
         return false;
     }
@@ -59,53 +62,43 @@ class model_client {
     {
         $db = model_database::instance();
         $sql = 'UPDATE clients
-                SET client_name =\'' . $name . '\', client_address = \'' . $address . '\', client_phone = \'' . $phone . '\'
-                WHERE client_id_account = \'' . $account_id . '\'
+                SET cliemt_of_account =\'' . $account_id . '\', client_name =\'' . mysql_real_escape_string($name) . '\', client_address = \'' . mysql_real_escape_string($address) . '\', client_phone = \'' . $phone . '\'
+                WHERE client_id_account = \'' . $this->account_id . '\'
                 limit 1';
         if($db->execute($sql)){
+            $this->account_id = $account_id;
+            $this->name = $name;
+            $this->address = $address;
+            $this->phone = $phone;
             return true;
         }
         return false;
     }
 
-    public static function delete($id){
+    public function delete(){
         $db = model_database::instance();
         $sql = 'DELETE FROM clients
-                where client_id_account = \'' . $id . '\';';
+                where client_id_account = \'' . $this->id . '\';';
         if($db->execute($sql)){
+            $this->id = null;
+            $this->account_id = null;
+            $this->name = null;
+            $this->address = null;
+            $this->phone = null;
             return true;
         }
     return false;
     }
 
-    public function get_account($id_account){
-        $db = model_database::instance();
-        $sql = 'SELECT * FROM accounts
-                WHERE account_id = \'' . $id_account . '\'';
-        if($result = $db->get_row($sql)){
-            $obj = new model_account();
-            $obj->account_id = $id_account;
-            $obj->username = $result['account_username'];
-            $obj->pass = $result['account_pass'];
-            $obj->type = $result['account_type'];
-            return $obj;
+    public function get_account(){
 
-        }
-        return false;
+        return model_account::load_by_id($this->account_id);
+
     }
 
     public function get_orders(){
-        $db = model_database::instance();
-        $sql = 'SELECT order_id, order_id_client, order_pickup_date FROM orders WHERE order_id = \'' . $this->id . '\'';
-        if ($rows = $db->get_rows($sql)){
-            $return = null;
-            foreach($rows as $row){
-                $orders = model_order::get_by_order_id($row['order_id']);
-                $return[] = $orders;
-            }
-            return $return;
-        }
-        return false;
+
+        return model_order::get_by_client_id($this->id);
     }
 }
 $model = model_client::load_by_id(3);
