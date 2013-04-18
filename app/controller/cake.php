712cf351f879d070a576d3cc4a3de5aad0071598
andra <andrada_corina@yahoo.com>
2013-04-18 11:03:27 +0300

    Update of action_list() function in controller and cake_list.tpl.php in view.

--- app/controller/cake.php
+++ app/controller/cake.php
@@ -6,15 +6,14 @@
  * Time: 10:29 AM
  * To change this template use File | Settings | File Templates.
  */
-include_once 'C:/wamp/www/cakestore/app/model/cake.php';
+@include_once 'C:/wamp/www/cakestore/app/model/cake.php';
 class controller_cake
 {
-    public static function  action_list()
+    public static function  action_list($params)
     {
         // Include view for this page
         $cakes = model_cake::get_all();
         @include_once APP_PATH . 'view/cake_list.tpl.php';
-        return $cakes;
         //$cakes = model_cake::get_all();
         //var_dump($cakes);
     }
--- app/view/cake_list.tpl.php
+++ app/view/cake_list.tpl.php
@@ -3,23 +3,24 @@
 <h2>Our cakes.</h2>
 
 <?php @include APP_PATH . '/view/snippets/footer.tpl.php'; ?>
-<?php include_once  "../controller/cake.php" ?>
-<?php $cls_a = new controller_cake(); ?>
-<?php $cakes = $cls_a::action_list(); ?>
+<?php// include_once  "../controller/cake.php" ?>
+<?php// $cls_a = new controller_cake(); ?>
+<?php// $cakes = $cls_a::action_list(); ?>
 <?php if ($cakes) : ?>
 
     <?php foreach ($cakes as $cake) : ?>
 
-        <p> <?php echo $cake->name;
-            echo " ";
+        <p><b><?php echo $cake->name;
+                    echo ' ';
+                    echo $cake->quantity; ?></b></p>
+        <p><<a href="<?php echo APP_URL ?>cake/view/<?php echo $cake->id
+            ?>">view cake details</a></p>
             echo $cake->quantity?>
-            <a href="cake/view/<?php echo $cake->id ?>">Cake page</a>
-        </p>
-
     <?php endforeach; ?>
 
 <?php else : ?>
 
-    <p>No cakes</p>
+    <p>Sorry, no sugar for you, babyyy!!</p>
 
 <?php endif; ?>
+<?php @include APP_PATH . '/view/snippets/footer.tpl.php'; ?>
