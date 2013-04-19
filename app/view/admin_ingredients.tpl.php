e31f33ea2a6ee85ffbf79a521c608212f787883a
phalien <phalien@Cristians-MacBook-Air.local>
2013-04-19 01:42:17 +0100

    MVC-14, MVC-15 Code review and small fixes + renamed adminn to admin

--- app/view/admin_ingredients.tpl.php
+++ app/view/admin_ingredients.tpl.php
@@ -2,15 +2,25 @@
 
 <h2>Ingredients</h2>
 
+<p><a href="<?php echo APP_URL ?>ingredient/add/">Add ingredient</a></p>
+
 <?php  if ($ingredients) : ?>
-    <a href="ingredient/add/">Add Ingredient</a></p>
+
+	<ol>
+
     <?php foreach ($ingredients as $ingredient) : ?>
 
-        <p><b><?php echo $ingredient->name; ?></b>
-        <a href="<?php echo APP_URL ?>ingredient/edit/<?php echo $ingredient->id
-            ?>">Edit ingredient</a></p>
+        <li>
+        	<h3><?php echo $ingredient->name; ?></h3>
+        	<ul>
+		        <li><a href="<?php echo APP_URL ?>ingredient/edit/<?php echo $ingredient->id ?>">Edit</a></li>
+		        <li><a href="<?php echo APP_URL ?>ingredient/delete/<?php echo $ingredient->id ?>">Delete</a></li>
+		    </ul>
+	    </li>
 
     <?php endforeach; ?>
+
+	</ol>
 
 <?php else : ?>
 
@@ -19,6 +29,3 @@
 <?php endif; ?>
 
 <?php @include APP_PATH . '/view/snippets/footer.tpl.php'; ?>
-
-
-
