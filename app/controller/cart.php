c45bf2326e7dfe92f6d80f3bee4f4ee7ab10b6eb
andra <andrada_corina@yahoo.com>
2013-04-19 14:30:08 +0300

    Implementing add to cart confirmation.

--- app/controller/cart.php
+++ app/controller/cart.php
@@ -7,13 +7,20 @@
  * To change this template use File | Settings | File Templates.
  */
 
-class controller_cart {
+class controller_cart
+{
 
     public function action_updated()
     {
         // Include view for this page
         @include_once APP_PATH . 'view/cart_updated.tpl.php';
 
+    }
+
+    public function action_added()
+    {
+        // Include view for this page
+        @include_once APP_PATH . 'view/cart_added.tpl.php';
     }
 
 }
---
+++ app/view/cart_added.tpl.php
@@ -0,0 +1,7 @@
+<?php @include APP_PATH . '/view/snippets/header.tpl.php'; ?>
+
+    <h2>The product was added to the cart.</h2>
+
+    <a href= <?php echo APP_URL . "cake/list" ?>>Back to cakesa list.</a>
+
+<?php @include APP_PATH . '/view/snippets/footer.tpl.php'; ?>
