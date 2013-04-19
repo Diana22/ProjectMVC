01f0b6df8dce97bf3c36f703236edde93f561a4e
Roman Alina <alina.roman22@yahoo.com>
2013-04-19 14:08:29 +0300

    MVC-55 Implement page: Edit client confirmation

---
+++ app/view/client_updated.tpl.php
@@ -0,0 +1,7 @@
+<?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
+
+    <h2>Client details were updated</h2>
+
+    <a href=<?php echo APP_URL ?>"/cakestore/app/model/account.php/"</a>;
+
+<?php @include APP_PATH . 'view/snippets/footer.tpl.php'; ?>
