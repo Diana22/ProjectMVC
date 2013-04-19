6ff85dce12088da7d5b0950782a11e7bdfea2128
roxana <roxana.rodina@yahoo.com>
2013-04-19 15:27:52 +0300

    MVC - 43 fix.

--- app/view/admin_index.tpl.php
+++ app/view/admin_index.tpl.php
@@ -1,11 +1,12 @@
 <?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
  <h2>Admin area</h2>
 
-    <a href= <?php APP_PATH ."cakestore/admin/ingredients/" ?> "/cakestore/admin/ingredients/">List of ingredients</a>
+
+    <a href="<?php echo APP_URL; ?>admin/ingredients/">List of ingredients</a>
     <br />
-    <a href= <?php APP_PATH ."cakestore/admin/list/" ?> "/cakestore/admin/list/">List of cakes</a>
+    <a href="<?php echo APP_URL; ?>cakestore/admin/list/">List of cakes</a>
     <br />
-    <a href= <?php APP_PATH ."cakestore/admin/orders/" ?> "/cakestore/admin/orders/">List of orders</a>
+    <a href="<?php echo APP_URL; ?>cakestore/admin/orders/">List of orders</a>
 
 
 <?php @include APP_PATH . 'view/snippets/footer.tpl.php'; ?>
