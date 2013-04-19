7b4416a91beb655ad846cec0cdab188d9d3e4cab
roxana <roxana.rodina@yahoo.com>
2013-04-19 15:01:04 +0300

    MVC - 43.

--- app/controller/admin.php
+++ app/controller/admin.php
@@ -1,9 +1,15 @@
 <?php
+@include_once "../model/ingredient.php";
 class controller_admin {
 
     public static function action_ingredients()
     {
         $ingredients = model_ingredient::get_all();
-        include APP_PATH . 'view/admin_ingredients.tpl.php';
+        @include APP_PATH . 'view/admin_ingredients.tpl.php';
+    }
+
+    public function action_index()
+    {
+        @include APP_PATH . 'view/admin_index.tpl.php';
     }
 }
