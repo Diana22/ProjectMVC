774ee46562a388f8f311feceaf1aa60b86849efc
andra <andrada_corina@yahoo.com>
2013-04-19 17:47:41 +0300

    Repost Ingredient delete confirmation.

--- app/controller/ingredient.php
+++ app/controller/ingredient.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Andrada
- * Date: 4/18/13
- * Time: 11:08 AM
- * To change this template use File | Settings | File Templates.
- */
 
 class controller_ingredient
 {
