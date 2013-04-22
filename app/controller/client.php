0626f246287db7d3afe87880a576dd5e2bcd0315
roxana <roxana.rodina@yahoo.com>
2013-04-22 12:22:53 +0300

    MVC - 54 Implement page: edit client.

--- app/controller/client.php
+++ app/controller/client.php
@@ -12,4 +12,16 @@ class controller_client {
         // Include view for this page
         include_once APP_PATH . 'view/client_notice.tpl.php';
     }
+
+    public function action_edit($params) {
+        $account_id = $_SESSION['myshop']['account_id'];
+        $client = model_client::load_by_id($account_id);
+        $form_error = FALSE;
+        if (isset($_POST['form']['action'])) {
+            $client->update($_POST['form']['name'], $_POST['form']['address'], $_POST['form']['phone'], $_POST['form']['account_id']);
+            header('Location: ' . APP_URL . 'client/updated');
+            $form_error = TRUE;
+        }
+        include APP_PATH . 'view/client_edit.tpl.php';
+    }
 }
