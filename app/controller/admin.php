52175f4c137229ec31acfb739f86f012a248102c
roxana <roxana.rodina@yahoo.com>
2013-04-22 15:52:44 +0300

    MVC - 57: Admin > orders.

--- app/controller/admin.php
+++ app/controller/admin.php
@@ -1,62 +1,21 @@
 <?php
-/**
- * Controller for handling admin user actions: login etc.
- */
+@include_once "../model/ingredient.php";
 class controller_admin {
 
-	/**
-	 * Main admin page.
-	 */
-	function action_index($params) {
+    public static function action_ingredients()
+    {
+        $ingredients = model_ingredient::get_all();
+        @include APP_PATH . 'view/admin_ingredients.tpl.php';
+    }
 
-		// Admin must be logged in to access this page
-		if (!($account = model_account::load_by_id($_SESSION['myshop']['account_id'])) || $account->type != model_account::TYPE_ADMIN) {
-			header('Location: ' . APP_URL . 'account/login/');
-			die;
-		}
+    public function action_index()
+    {
+        @include APP_PATH . 'view/admin_index.tpl.php';
+    }
 
-		// Include view for this page
-		@include_once APP_PATH . 'view/admin_index.tpl.php';
-	}
-
-	/**
-	 * Login page for admin.
-	 */
-	function action_login($params) {
-
-		// If the form was submitted, validate credentials
-		$form_error = FALSE;
-		if (isset($_POST['form']['action'])) {
-			if ($admin_user_id = model_admin::validate($_POST['form']['user'], $_POST['form']['password'])) {
-				$_SESSION['myshop']['admin_user_id'] = $admin_user_id;
-				header('Location: ' . APP_URL . 'admin');
-				die;
-			}
-			$form_error = TRUE;
-		}
-
-		// Include view for this page
-		@include_once APP_PATH . 'view/admin_login.tpl.php';
-	}
-
-
-	/**
-	 * Lists ingredients.
-	 */
-	function action_ingredients($params) {
-
-		$ingredients = model_ingredient::get_all();
-
-		// Include view for this page
-		@include_once APP_PATH . 'view/admin_ingredients.tpl.php';
-	}
-
-	/**
-	 * Logout action.
-	 */
-	function action_logout($params) {
-		unset($_SESSION['myshop']['admin_user_id']);
-		header('Location: ' . APP_URL . 'admin/login');
-		die;
-	}
+    public function action_orders()
+    {
+        $orders = model_order::get_all();
+        @include APP_PATH . 'view/admin_orders.tpl.php';
+    }
 }
---
+++ app/view/admin_orders.tpl.php
@@ -0,0 +1,20 @@
+<?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
+
+<h2>Orders</h2>
+
+<?php  if ($orders) : ?>
+    <?php foreach ($orders as $order) : ?>
+
+        <p><b><?php echo $order->id; ?></b>
+            <a href="<?php echo APP_URL ?>order/edit/<?php echo $order->id ?>">  Edit order</a>
+        </p>
+
+    <?php endforeach; ?>
+
+<?php else : ?>
+
+    <p>No orders</p>
+
+<?php endif; ?>
+
+<?php @include APP_PATH . '/view/snippets/footer.tpl.php'; ?>
