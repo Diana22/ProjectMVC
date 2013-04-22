93a6add2203eb7c351a84e8cc042f0a8809208cc
roxana <roxana.rodina@yahoo.com>
2013-04-22 11:13:26 +0300

    MVC - 35 Implement page: Edit ingredient.

--- app/controller/ingredient.php
+++ app/controller/ingredient.php
@@ -1,44 +1,21 @@
 <?php
+@include_once "../model/ingredient.php";
+class controller_ingredient {
 
-class controller_ingredient
-{
-
-    public static function action_updated()
-    {
+    public static function action_updated() {
         include APP_PATH . 'view/ingredient_updated.tpl.php';
     }
 
-    public function action_edit($params)
-    {
-        $ingredient = model_ingredient::load_by_id($params[0]);
+    public function action_edit($params) {
+        $e = $params[0];
+        $ingredient = model_ingredient::load_by_id($e);
         $form_error = FALSE;
         if (isset($_POST['form']['action'])) {
             $ingredient->edit($_POST['form']['name']);
             header('Location: ' . APP_URL . 'ingredient/updated');
             $form_error = TRUE;
         }
-
         include APP_PATH . 'view/ingredient_edit.tpl.php';
 
-    }
-
-    public function action_delete($params)
-    {
-
-        $ingredient = model_ingredient::load_by_id($params[0]);
-
-        //Check if form was submitted.
-        if (isset($_POST['form']['action'])) {
-            $ingredient->delete();
-            header('Location: ' . APP_URL . 'ingredient/deleted');
-        }
-        // Include view for this page.
-        @include_once APP_PATH . 'view/ingredient_delete.tpl.php';
-    }
-
-    public static function  action_deleted($params)
-    {
-        // Include view for this page
-        @include_once APP_PATH . 'view/ingredient_deleted.tpl.php';
     }
 }
---
+++ app/view/ingredient_edit.tpl.php
@@ -0,0 +1,23 @@
+<?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
+
+<h2>Edit ingredient</h2>
+<?php if ($form_error) : ?>
+    <p>Ceva e gresit. Reincercati.</p>
+<?php endif ?>
+
+<form action="<?php echo APP_URL . "ingredient/edit/" . $ingredient->id; ?>" method="post">
+    <label>
+        <input type="hidden" name="form[id]" value="<?php $ingredient->id ?>"/>
+    </label>
+    <label>Nume
+        <input type="text" name="form[name]" value="<?php echo $ingredient->name ?>"/>
+    </label>
+    <br/>
+    <input type="submit" name="form[action]" value="Update"/>
+</form>
+
+<?php @include APP_PATH . 'view/snippets/footer.tpl.php'; ?>
+
+
+
+
