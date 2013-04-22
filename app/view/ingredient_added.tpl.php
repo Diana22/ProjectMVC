5f83ac036ca7505db6965d6b19948446f09276cc
roxana <roxana.rodina@yahoo.com>
2013-04-22 11:20:20 +0300

    MVC - 67 Implement page: Add ingredient confirmation.

---
+++ app/view/ingredient_added.tpl.php
@@ -0,0 +1,6 @@
+<?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
+
+<h2>The ingredient was added</h2>
+    <a href= <?php APP_PATH ."/admin/ingredients/" ?> "../admin/ingredients/">List of ingredients</a>
+
+<?php @include APP_PATH . '/view/snippets/footer.tpl.php'; ?>
