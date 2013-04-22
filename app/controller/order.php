b42d87ae263adef230a54287e813657a8aa07526
andra <andrada_corina@yahoo.com>
2013-04-22 12:58:42 +0300

    Implement page: Edit order.

--- app/controller/order.php
+++ app/controller/order.php
@@ -1,8 +1,27 @@
 <?php
-class controller_order {
+class controller_order
+{
 
     public function action_updated()
     {
         include APP_PATH . 'view/order_updated.tpl.php';
+    }
+
+    /**
+     * This function edits an order.
+     * @param $params
+     */
+    public function action_edit($params)
+    {
+        @include_once APP_PATH . '/model/order.php';
+        $order = model_order::get_by_order_id($params[0]);
+
+        if (isset($_POST['form']['action'])) {
+            $order->update($_POST['form']['id_client'], $_POST['form']['pickup_date']);
+            header('Location: ' . APP_URL . 'order/updated/');
+
+        }
+        @include APP_PATH . 'view/order_edit.tpl.php';
+
     }
 }
---
+++ app/view/order_edit.tpl.php
@@ -0,0 +1,17 @@
+<?php @include APP_PATH . 'view/snippets/header.tpl.php'; ?>
+
+    <form action=<?php echo APP_URL; ?>order/edit/<?php echo $order->id; ?> method="post">
+
+        <label>Client ID
+            <input type="text" name="form[id_client]" value=<?php echo $order->id_client; ?>>
+        </label><br/>
+
+        <label>Pickup Date
+            <input type="text" name="form[pickup_date]" value=<?php echo $order->pickup_date; ?>>
+        </label><br/>
+
+        <input type="hidden" name="form[id]" value=<?php echo $order->id; ?>>
+        <input type="submit" name="form[action]" value="Update">
+    </form>
+
+<?php @include APP_PATH . '/view/snippets/footer.tpl.php'; ?>
