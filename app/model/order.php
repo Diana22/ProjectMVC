957999140d4087203c8fe7c864076d7ad7bf30be
Roman Alina <alina.roman22@yahoo.com>
2013-04-23 16:14:13 +0300

    MVC-72 Add status field to order model

--- app/model/order.php
+++ app/model/order.php
@@ -2,21 +2,23 @@
 include_once __DIR__ . "/database.php";
 include_once __DIR__ . "/cake.php";
 include_once __DIR__ . "/client.php";
-class model_order
-{
+class model_order {
 
+    const type_cancel = -1;
+    const type_pending = 0;
+    const type_process = 1;
+
+    var $status;
     var $id;
     var $client_id;
     var $pickup_date;
-
 
     /*
      * Gets the order that has a specific id.
      * @param $id id to search for
      * @return a specific order.
      */
-    public static function load_by_id($id)
-    {
+    public static function load_by_id($id){
         $db = model_database::instance();
         $sql = 'SELECT *
 			    FROM orders
@@ -24,7 +26,7 @@ class model_order
 			    LIMIT 1;';
         if ($result = $db->get_row($sql)) {
             $return = new model_order();
-            $return->set_properties($result['order_id'], $result['order_id_client'], $result['order_pickup_date']);
+            $return->set_properties($result['order_id'],$result['order_id_client'],$result['order_pickup_date'],$result['order_status']);
             return $return;
         }
         return false;
@@ -35,17 +37,17 @@ class model_order
     * @param $id id to search for
     * @return array of orders made by client.
     */
-    public static function get_by_client_id($id)
-    {
+    public static function get_by_client_id($id){
         $db = model_database::instance();
         $sql = 'SELECT *
 			    FROM orders
 			    WHERE order_id_client = ' . $id . ';';
         if ($result = $db->get_rows($sql)) {
             $return = array();
-            foreach ($result as $array) {
+            foreach ($result as $array)
+            {
                 $model = new model_order();
-                $model->set_properties($array['order_id'], $array['order_id_client'], $array['order_pickup_date']);
+                $model->set_properties($array['order_id'],$array['order_id_client'],$array['order_pickup_date'],$array['order_status']);
                 $return[] = $model;
             }
             return $return;
@@ -53,33 +55,33 @@ class model_order
         return false;
     }
 
-    /*
-    * Adds an order.
-    * @param $id client id.
-    * @param $pickup date in format yyyy.mm.yy.
-    */
-    public static function create($client_id, $date)
-    {
+     /*
+     * Adds an order.
+     * @param $id client id.
+     * @param $pickup date in format yyyy.mm.yy.
+     */
+    public static function create($client_id, $date, $status){
         $db = model_database::instance();
         $sql = 'INSERT INTO orders
-                    (order_id_client, order_pickup_date)
+                    (order_id_client, order_pickup_date, order_status)
                 VALUES
-                    (' . $client_id . ',\'' . $date . '\');';
-        if ($db->execute($sql)) {
-            return model_order::load_by_id($db->last_insert_id());
+                    (' . $client_id . ',\'' . $date . ',\'' . $status . ',\');';
+        if ($db->execute($sql))
+        {
+            $new_status = $db->last_insert_id();
+            return model_order::load_by_id($new_status);
         }
         return false;
     }
 
-    /*
-    * Deletes an order the entry corresponding to the current object.
-    */
-    public function delete()
-    {
+     /*
+     * Deletes an order the entry corresponding to the current object.
+     */
+    public function delete(){
         $db = model_database::instance();
         $sql = 'DELETE FROM orders
                 WHERE order_id=\'' . $this->id . '\';';
-        if (!$db->execute($sql)) {
+        if(!$db->execute($sql)){
             return false;
         }
 
@@ -90,21 +92,22 @@ class model_order
         $this->id = null;
         $this->client_id = null;
         $this->pickup_date = null;
+        $this->status = null;
         return true;
     }
 
     /*
      * Updates the current record with new values.
      */
-    public function update($client_id, $pickup_date)
-    {
+    public function update($client_id, $pickup_date, $status) {
         $db = model_database::instance();
         $sql = 'UPDATE orders
-                SET order_id_client=' . $client_id . ', order_pickup_date=\'' . $pickup_date . '\'
+                SET order_id_client=' . $client_id . ', order_pickup_date=\'' . $pickup_date . '\', order_status=\'' . $status . '\'
                 WHERE order_id=' . $this->id;
-        if ($db->execute($sql)) {
+        if($db->execute($sql)){
             $this->client_id = $client_id;
-            $this->pickup_date = substr($pickup_date,0,10);
+            $this->pickup_date = $pickup_date;
+            $this->status = $status;
             return true;
         }
         return false;
@@ -113,12 +116,11 @@ class model_order
     /*
      * Adds new items to the current order.
      */
-    public function add_cake($cake_id, $quantity)
-    {
+    public function add_cake($cake_id, $quantity){
         $db = model_database::instance();
         $sql = 'INSERT INTO orders_cakes
-                VALUES (' . $this->id . ',' . $cake_id . ',' . $quantity . ')';
-        if ($db->execute($sql)) {
+                VALUES ('. $this->id . ',' . $cake_id . ',' . $quantity . ')';
+        if ($db->execute($sql)){
             return true;
         }
         return false;
@@ -127,31 +129,29 @@ class model_order
     /*
      * Deletes specified cake from current order.
      */
-    public function remove_cake($cake_id)
-    {
+    public function remove_cake($cake_id){
         $db = model_database::instance();
         $sql = 'DELETE FROM orders_cakes
                 WHERE oc_id_order=' . $this->id . ' AND oc_id_cake=' . $cake_id . ';';
-        if ($db->execute($sql)) {
+        if ($db->execute($sql)){
             return true;
         }
         return false;
     }
 
     /*
-     * Returns all cakes within the current order and their quantities.
+     * Returnes all cakes within the current order and their quantities.
      */
-    public function get_cakes()
-    {
+    public function get_cakes(){
         $db = model_database::instance();
         $sql = 'SELECT *
                 FROM orders_cakes
                 WHERE oc_id_order=' . $this->id . ';';
-        if ($rows = $db->get_rows($sql)) {
+        if ($rows = $db->get_rows($sql)){
             $cakes = null;
-            foreach ($rows as $row) {
+            foreach($rows as $row){
                 $cake = model_cake::load_by_id($row['oc_id_cake']);
-                $cake->ordered_quantity = $row['oc_quantity'];
+                $cake->ordered_quantity = $row['oc_quantity'] ;
                 $cakes[] = $cake;
             }
             return $cakes;
@@ -162,19 +162,18 @@ class model_order
     /*
      * Returns a client object that is linked to current order.
      */
-    public function get_client()
-    {
+    public function get_client(){
         return model_client::load_by_id($this->client_id);
     }
 
     /*
      * Sets the object attributes to those of an array with same keys.
      */
-    public function set_properties($order_id, $order_id_client, $order_pickup_date)
-    {
+    public function set_properties($order_id,$order_id_client,$order_pickup_date, $status){
         $this->id = $order_id;
         $this->client_id = $order_id_client;
-        $this->pickup_date = substr($order_pickup_date,0,10);
+        $this->pickup_date = $order_pickup_date;
+        $this->status = $status;
     }
 
     /*
@@ -192,11 +191,20 @@ class model_order
                 $orders->id = $array['order_id'];
                 $orders->client_id = $array['order_id_client'];
                 $orders->pickup_date = substr($array['order_pickup_date'],0,10);
+                $orders->status = $array['status'];
                 $return[] = $orders;
             }
             ksort($return);
         }
         ;
         return $return;
+    }
+
+    public function set_status($status){
+        $this->status = $status;
+    }
+
+    public function get_status($staus){
+        return $this->status;
     }
 }
