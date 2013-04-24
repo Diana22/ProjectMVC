93509242d277b69b8f1d6c717fe9b0f8ec7ac18c
Priceputu Victor Ovidiu <price_832@yahoo.com>
2013-04-24 15:01:56 +0300

    MVC-76 modified index and all functions.

--- app/controller/admin.php
+++ app/controller/admin.php
@@ -8,42 +8,57 @@ class controller_admin {
 	 * Main admin page.
 	 */
 	function action_index($params) {
-
-		// Admin must be logged in to access this page
-		if (isset($_SESSION) && $_SESSION['myshop']['account_type'] != "admin") {
-			header('Location: ' . APP_URL . 'account/login/');
-			die;
-		}
-
-		// Include view for this page
+        if (model_validate::validation()){
 		@include_once APP_PATH . 'view/admin_index.tpl.php';
+        }
+        else
+        {
+            @include_once APP_PATH . 'view/404_admin.tpl.php';
+        }
 	}
 
 	/**
 	 * Lists ingredients.
 	 */
 	function action_ingredients($params) {
-
-		$ingredients = model_ingredient::get_all();
-
-		// Include view for this page
-		@include_once APP_PATH . 'view/admin_ingredients.tpl.php';
+        if (model_validate::validation()){
+            $ingredients = model_ingredient::get_all();
+            @include_once APP_PATH . 'view/admin_ingredients.tpl.php';
+        }
+        else @include_once APP_PATH . 'view/404_admin.tpl.php';
 	}
 
+    /*
+     * Lists orders.
+     */
     function action_orders($params)
     {
-        $orders = model_order::get_all();
-        @include APP_PATH . 'view/admin_orders.tpl.php';
+        if (model_validate::validation()){
+            $orders = model_order::get_all();
+            @include APP_PATH . 'view/admin_orders.tpl.php';
+        }
+        else @include_once APP_PATH . 'view/404_admin.tpl.php';
     }
 
+    /*
+     * Lists all cakes.
+     */
     function action_cakes($params){
-        $cakes = model_cake::get_all();
-        // Include view for this page
-        @include_once APP_PATH . 'view/admin_cake_list.tpl.php';
+        if (model_validate::validation()){
+            $cakes = model_cake::get_all();
+            // Include view for this page
+            @include_once APP_PATH . 'view/admin_cake_list.tpl.php';
+        }
+        else @include_once APP_PATH . 'view/404_admin.tpl.php';
     }
 
+    /*
+     */
     function action_cake($params){
-        $cake = model_cake::load_by_id($params[0]);
-        @include_once APP_PATH . 'view/admin_cake_view.tpl.php';
+        if (model_validate::validation()){
+            $cake = model_cake::load_by_id($params[0]);
+            @include_once APP_PATH . 'view/admin_cake_view.tpl.php';
+        }
+        else @include_once APP_PATH . 'view/404_admin.tpl.php';
     }
 }
